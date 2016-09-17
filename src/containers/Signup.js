733e78fc4fb8c5c983a92e4528ab820381d41b88
Pavel Prichodko <prichodko.p@gmail.com>
2016-09-17 11:23:43 +0200

    Add sign up container

---
+++ src/containers/Signup.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+
+import Logo from '../assets/logo.svg'
+import TeamForm from '../components/signup/TeamForm'
+import NameForm from '../components/signup/NameForm'
+import Overview from '../components/signup/Overview'
+import PasswordForm from '../components/signup/PasswordForm'
+
+import './Singup.css'
+
+class Signup extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      step: 0,
+      data: { teamname: '', firstname: '', lastname: '', password: '' }
+    }
+
+    this.handleDataSubmit = this.handleDataSubmit.bind(this)
+  }
+
+  getForm (step) {
+    const forms = [
+      { component: () => <TeamForm onDataSubmit={this.handleDataSubmit} /> },
+      { component: () => <NameForm onDataSubmit={this.handleDataSubmit} /> },
+      { component: () => <Overview data={this.state.data} onDataSubmit={this.handleDataSubmit} /> },
+      { component: () => <PasswordForm /> }
+    ]
+
+    return forms[step].component()
+  }
+
+  handleDataSubmit (data) {
+    const updated = Object.assign({}, this.state.data, data)
+    this.setState({ step: ++this.state.step, data: updated })
+  }
+
+  render () {
+    console.log(this.state)
+    return (
+      <div className='signup-container'>
+        <div className='signup-main'>
+          <div className='main-header'>
+            <img src={Logo} className='main-header__logo' />
+          </div>
+          {this.getForm(this.state.step)}
+        </div>
+        <div className='signup-secondary' />
+      </div>
+    )
+  }
+}
+
+export default Signup
