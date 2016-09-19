aab0bfe0f3bc0ac7f39ea7da22515c245e1729b2
Pavel Prichodko <prichodko.p@gmail.com>
2016-09-19 18:44:15 +0200

    Add main app components

---
+++ src/containers/App/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+import Pane from '../../components/app/Pane'
+import Canvas from '../../components/app/Canvas'
+
+import './App.css'
+
+class App extends Component {
+  render () {
+    return (
+      <div className='app-container'>
+        <Pane />
+        <Canvas />
+      </div>
+    )
+  }
+}
+
+export default App
