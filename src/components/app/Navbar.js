d7361d785363e6da3c7e9ec412790abebd9a17f3
Pavel Prichodko <prichodko.p@gmail.com>
2016-09-20 17:00:18 +0200

    Add tags for better layout

--- src/components/app/Navbar.js
+++ src/components/app/Navbar.js
@@ -1,17 +1,34 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
 
+import Plus from '../../assets/plus.svg'
+
 class Navbar extends Component {
   render () {
     return (
       <div className='navbar'>
         <div className='navbar__teamname'>
-          Ordr team Prague
+          <div>
+            Ordr team Prague
+          </div>
         </div>
-        <Link to='/app' className='navbar__item'>Shifts</Link>
-        <Link to='/app/team' className='navbar__item'>Team</Link>
-        <Link to='/app/payroll' className='navbar__item'>Payroll</Link>
-        <div className='navbar__add-button'>+</div>
+        <ul className='navbar__list'>
+          <li className='navbar__item'>
+            <Link to='/app' className='navbar__link'> Shifts
+            </Link>
+          </li>
+          <li className='navbar__item'>
+            <Link to='/app/team' className='navbar__link'> Team
+            </Link>
+          </li>
+          <li className='navbar__item'>
+            <Link to='/app/payroll' className='navbar__link'> Payroll
+            </Link>
+          </li>
+        </ul>
+        <div className='navbar__add-button'>
+          <img src={Plus} />
+        </div>
       </div>
     )
   }
