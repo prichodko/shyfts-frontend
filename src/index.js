285f8ca694fdc17c00b9227f294b34e840a9d226
Pavel Prichodko <prichodko.p@gmail.com>
2016-09-24 15:53:03 +0200

    Routes are not implemented yet

--- src/index.js
+++ src/index.js
@@ -3,18 +3,21 @@ import ReactDOM from 'react-dom'
 
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 
-import App from './containers/App'
-import Signup from './containers/Signup'
+import Signup from './containers/Signup/Signup'
+
+import App from './containers/App/App'
+// import Calendar from './containers/Calendar'
+// import Team from './containers/Team'
+// import Payroll from './containers/Payroll'
+    // <IndexRoute component={Calendar} />
+   // <Route path='team' component={Team} />
+   // <Route path='payroll' component={Payroll} />
 
 import './index.css'
 
 ReactDOM.render(
   <Router history={browserHistory}>
-    <Route path='/app' component={App}>
-      <IndexRoute component={Calendar} />
-      <Route path='team' component={Team} />
-      <Route path='payroll' component={Payroll} />
-    </Route>
+    <Route path='/app' component={App} />
     <Route path='/signup' component={Signup} />
   </Router>,
   document.getElementById('root')
