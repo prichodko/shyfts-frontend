f02b5f6f5415de850001bce812d18b08638ea72f
Pavel Prichodko <prichodko.p@gmail.com>
2016-09-24 14:23:10 +0200

    Merged changes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router, Route, browserHistory } from 'react-router'
-import App from './containers/App/App'
-import Signup from './containers/Signup/Signup'
+
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+
+import App from './containers/App'
+import Signup from './containers/Signup'
 
 import './index.css'
 
 ReactDOM.render(
   <Router history={browserHistory}>
-    <Route path='/app' component={App} />
-    <Route path='/' component={Signup} />
+    <Route path='/app' component={App}>
+      <IndexRoute component={Calendar} />
+      <Route path='team' component={Team} />
+      <Route path='payroll' component={Payroll} />
+    </Route>
+    <Route path='/signup' component={Signup} />
   </Router>,
   document.getElementById('root')
 )
