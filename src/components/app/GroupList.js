78644a90c8c995d304c6b66bea80be9981d5b072
Pavel Prichodko <prichodko.p@gmail.com>
2016-09-25 13:04:40 +0200

    Extract static data

--- src/components/app/GroupList.js
+++ src/components/app/GroupList.js
@@ -1,20 +1,12 @@
 import React, { Component } from 'react'
 
 import GroupItem from './GroupItem'
+import roles from '../../data/roles'
 
-const groups = [
-  { title: 'Green Group', color: '#3DD281' },
-  { title: 'Blue Group', color: '#4094FC' },
-  { title: 'Red Group', color: '#E63657' },
-  { title: 'Orange Group', color: '#FBA426' },
-  { title: 'Yellow Group', color: '#FEE000' },
-  { title: 'Purple Group', color: '#6052FF' },
-  { title: 'Levander Group', color: '#BC86FF' }
-]
 
 class GroupList extends Component {
   render () {
-    const items = groups.map((value, index) => <GroupItem key={index} color={value.color}> {value.title} </GroupItem>)
+    const items = roles.map((value, index) => <GroupItem key={index} color={value.color}> {value.role} </GroupItem>)
     return (
       <ul className='group-list'>
         {items}
