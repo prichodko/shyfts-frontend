8512aa16ee5b10b07fb03e32df95773502ddfea7
Pavel Prichodko <prichodko.p@gmail.com>
2016-09-28 15:10:52 +0200

    Dummy events data

---
+++ src/data/events.js
@@ -0,0 +1,25 @@
+// import moment from 'moment'
+
+export default [
+  {
+    'title': 'Shift',
+    'start': new Date(2016, 8, 27, 10, 30, 0),
+    'end': new Date(2016, 8, 27, 16, 30, 0),
+    'assigned': true,
+    'people': 10
+  },
+  {
+    'title': 'Shift',
+    'start': new Date(2016, 8, 26, 12, 30, 0),
+    'end': new Date(2016, 8, 26, 18, 30, 0),
+    'assigned': false,
+    'people': 10
+  },
+  {
+    'title': 'Shift',
+    'start': new Date(2016, 8, 26, 18, 35, 0),
+    'end': new Date(2016, 8, 26, 22, 30, 0),
+    'assigned': false,
+    'people': 10
+  }
+]
